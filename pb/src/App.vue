5c64c75831e79d2ee90491377ae748dbbc1b8d6c
Lera Matieva <foystina@gmail.com>
2020-09-20 00:24:11 +0400

    initial commit

---
+++ pb/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <Analytics />
+  </div>
+</template>
+
+<script>
+
+import Analytics from './components/Analytics'
+export default {
+  name: 'App',
+  components: {
+    Analytics
+  },
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+  //     this.$axios.post('https://api.cldcnt.ru/v1/offer/indexProduct', {
+  //       "productUrls":[
+  //       "https://www.ozon.ru/product/gazirovannyy-napitok-baltika-pivo-0-bezalkogolnoe-svetloe-pasterizovannoe-0-5-0-47-l-180887123",
+  //       "https://www.ozon.ru/context/detail/id/165063472/",
+  //       "https://www.ozon.ru/product/gazirovannyy-napitok-baltika-pshenichnoe-0-pivnoy-bezalkogolnyy-nefiltrovannyy-neosvetlennyy-180887125",
+  //       "https://www.ozon.ru/product/gazirovannyy-napitok-baltika-pivo-0-svetloe-bezalkogolnoe-pasterizovannoe-0-5-0-45-l-180887124",
+  //       "https://www.ozon.ru/product/bezalkogolnoe-pivo-bud-pasterizovannoe-0-45-l-upakovka-24-sht-173627596"
+  //   ],
+  //       "apiKey":"ajhFHGDkdnerGDDERHdjns43jke",
+  // 
+  //     })
+  //       .then(response => {
+  //         this.test = response;
+  //         console.log(response)
+  //       }) 
+   }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #f3f3f3;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ pb/src/components/Analytics.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="container">
+        <aside>
+            <div class="select">
+                <fieldset class="select-group">
+                <legend class="select-label">Бренд</legend>
+                <select class="select-list" name="select-brand" id="brand">
+                    <option value="all">Все</option>
+                    <option value=""></option>
+                </select>
+                </fieldset>
+            </div>
+            
+            <div class="select">
+                <fieldset class="select-group">
+                <legend class="select-label">Категория</legend>
+                <select class="select-list" name="select-brand" id="brand">
+                    <option value="all">Все</option>
+                    <option value=""></option>
+                </select>
+            </fieldset>
+            </div>
+
+            <div class="select">
+                <fieldset class="select-group">
+                <legend class="select-label">Магазин</legend>
+                <select class="select-list" name="select-brand" id="brand">
+                    <option value="all">Все</option>
+                    <option value=""></option>
+                </select>
+            </fieldset>
+            </div>
+            
+            <div class="select">
+                <fieldset class="select-group">
+                <legend class="select-label">Дата обновления</legend>
+                <select class="select-list" name="select-brand" id="brand">
+                    <option value="all">Все</option>
+                    <option value=""></option>
+                </select>
+            </fieldset>
+            </div>
+
+            <div class="select">
+                <fieldset class="select-group">
+                <legend class="select-label">SKU</legend>
+                <select class="select-list" name="select-brand" id="brand">
+                    <option value="all">Все</option>
+                    <option value=""></option>
+                </select>
+            </fieldset>
+            </div>
+        </aside>
+        <div class="main">
+            <div class="one">Доля бренда в категории</div>
+            <div class="two">Представлено в магазинах</div>
+            <div class="three">Наличие в магазинах</div>
+            <div class="four">Количество раз в стопе</div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Analytics'
+}
+</script>
+
+<style scoped>
+* {
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
+}
+.container {
+    display: flex;
+    background-color: #f3f3f3;
+    height: 400px;
+}
+aside {
+    width: 300px;
+    font-size: 14px;
+}
+
+.one {
+    grid-area: one;
+    background-color: #ffffff;
+}
+
+.two {
+    grid-area: two;
+    background-color: #ffffff;
+}
+
+.three {
+    grid-area: three;
+    background-color: #ffffff;
+}
+
+.four {
+    grid-area: four;
+    background-color: #ffffff;
+}
+
+.main {
+    margin: 10px;
+    font-size: 14px;
+    font-weight: bold;
+    display: grid;
+    grid-template-columns: 250px 250px 450px;
+    grid-template-areas: 
+        "one two three"
+        "one four three"; 
+    grid-gap: 10px;
+    height: 290px;
+}
+
+.select {
+    width: 95%;
+    height: 65px;
+    background-color: #ffffff;
+    margin: 10px auto;
+    padding-top: 5px;
+}
+
+.select-group {
+    height: 60px;
+    display: flex;
+    justify-content: flex-start;
+    font-weight: bold;
+    padding-left: 15px;
+    padding-top: 10px;
+    border: 0 none;
+}
+
+.select-label {
+    display: flex;
+    text-align: left;
+}
+
+.select-list {
+    width: 98%;
+    height: 25px;
+}
+</style>
